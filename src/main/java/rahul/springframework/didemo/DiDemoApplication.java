1202b3f2bf7f9f85bc08090ab0a5793058d83d9e
RahulD <rahul.deore@hotmail.com>
2019-09-10 20:58:12 -0400

    Added Spring properties injection through classpath properties file. Not using PropertySourcePlaceholderConfigurer for now. No difference observed even when using.

--- src/main/java/rahul/springframework/didemo/DiDemoApplication.java
+++ src/main/java/rahul/springframework/didemo/DiDemoApplication.java
@@ -5,10 +5,8 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import rahul.springframework.didemo.controller.ConstructorInjectedController;
 import rahul.springframework.didemo.controller.MyController;
-import rahul.springframework.didemo.controller.PropertyInjectedController;
-import rahul.springframework.didemo.controller.SetterInjectedController;
+import rahul.springframework.didemo.examplebeans.FakeDataSource;
 
 @SpringBootApplication
 //@ComponentScan(basePackages = {"rahul.controller","rahul.services"})
@@ -18,15 +16,16 @@ public class DiDemoApplication implements InitializingBean, DisposableBean {
     public void afterPropertiesSet() throws Exception {
         System.out.println("Properties are set for the beans");
         System.out.println();
+        //ApplicationContext context = SpringApplication.run(DiDemoApplication.class); Never do this. It creates endless loop
     }
 
     public static void main(String[] args) {               
         ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
         MyController myController = (MyController) ctx.getBean("myController");
-        System.out.println(myController.hello());
-        System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
-        System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
-        System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+
+        FakeDataSource fakeDataSource = ctx.getBean(FakeDataSource.class);
+        System.out.println(fakeDataSource.getUser() + " " + fakeDataSource.getPass() + " " + fakeDataSource.getUr());
+
     }
 
 
