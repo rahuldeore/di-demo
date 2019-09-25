f755bdcbe9a798254810a573fcee4d27dae16680
RahulD <rahul.deore@hotmail.com>
2019-09-25 19:59:54 -0400

    Removed the custom property files and added the properties to spring's default application.properties. Some refactoring in configuration class.

--- src/main/java/rahul/springframework/didemo/config/PropertyConfig.java
+++ src/main/java/rahul/springframework/didemo/config/PropertyConfig.java
@@ -1,12 +1,8 @@
 package rahul.springframework.didemo.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.core.env.Environment;
 import rahul.springframework.didemo.examplebeans.FakeDataSource;
 import rahul.springframework.didemo.examplebeans.FakeJmsBroker;
 
@@ -14,15 +10,7 @@ import rahul.springframework.didemo.examplebeans.FakeJmsBroker;
  * Created by Rahul on 2019-09-10
  */
 @Configuration
-//@PropertySource({"classpath:datasource.properties","classpath:jms.properties"})
-@PropertySources({
-        @PropertySource("classpath:datasource.properties"),
-        @PropertySource("classpath:jms.properties")
-})
 public class PropertyConfig {
-
-    @Autowired
-    Environment env;
 
     @Value("${guru.username}")
     String user;
@@ -45,9 +33,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
-        //fakeDataSource.setUser(user);
-        //fakeDataSource.setUser(env.getProperty("logname"));
-        fakeDataSource.setUser(env.getProperty("username"));
+        fakeDataSource.setUser(user);
         fakeDataSource.setPass(pass);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
@@ -62,9 +48,4 @@ public class PropertyConfig {
         return fakeJmsBroker;
     }
 
-   /* @Bean
-    public static PropertySourcesPlaceholderConfigurer properties(){
-        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-        return propertySourcesPlaceholderConfigurer;
-    }*/
 }
